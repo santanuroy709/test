34aead3ea307e1318d2b7b8a69b22cd6c3bcb366
santanu <santanu.roy709@gmail.com>
2022-11-18 15:43:12 +0530

    master brancge committedd

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 print("hello world")
+<<<<<<< HEAD
 print("Hello Santanu")
+=======
+>>>>>>> santanu
 print("sdfsd")
 print("hello world")
 print("sdfsd")
 print("hello world")
 print("sdfsd")
 print("hello world")
+<<<<<<< HEAD
 print("sdfsd")
+=======
+print("sdfsd")
+>>>>>>> santanu
